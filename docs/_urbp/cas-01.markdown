1e4476bc4d20cd370804651968483c2bdc9d7d6f
Aca-S <a.stefanovic1999@gmail.com>
2024-10-08 21:30:50 +0200

    Added lecture 1 materials

--- docs/_urbp/cas-01.markdown
+++ docs/_urbp/cas-01.markdown
@@ -4,5 +4,7 @@ title: Čas 1 - Uvod u SQL
 ---
 
 ### Materijali
+- [Zadaci sa časa]({{ site.baseurl }}{% link resources/urbp/01_zadaci_sa_casa.sql %})
+- [Zadaci za vežbu]({{ site.baseurl }}{% link resources/urbp/01_zadaci_za_vezbu.sql %})
 
 ### Uputstva
---
+++ docs/resources/urbp/01_zadaci_za_vezbu.sql
@@ -0,0 +1,36 @@
+-- 1. Izdvojiti indeks, ime i prezime za svakog studenta koji je upisao fakultet.
+
+
+-- 2. Izdvojiti sve razlicite vrednosti u koloni espb u tabeli predmet.
+
+
+-- 3. Izdvojiti sva zenska imena studenata.
+
+
+-- 4. Izdvojiti sva zenska imena studenata koja se ne zavrsavaju slovom a.
+
+
+-- 5. Izdvojiti podatke o ispitima koji su odrzani u ispitnom roku sa oznakom jan1 i na kojima je student dobio 100 poena.
+
+
+-- 6. Izdvojiti indekse studenata koji su na nekom ispitu dobili izmedju 65 i 87 poena.
+
+
+/*
+7. Izdvojiti indeks i godinu upisa na fakultet za studente koji fakultet nisu upisali izmedju 2013. i 2016. godine.
+Kolonu sa godinom upisa nazvati Godina upisa, a kolonu sa indeksom Student. Koristiti pretpostavku da godina iz indeksa
+odgovara godini upisa na fakultet.
+*/
+
+
+/*
+8. Izdvojiti indeks, ime i godinu upisa na fakultet za studente koji fakultet nisu upisali izmedju 2013. i 2016. godine
+i cije ime pocinje na slovo M, trece slovo u imenu je r, a zavrsava se na a. Rezultat urediti prema imenu studenta
+u rastucem poretku.
+*/
+
+
+/*
+9. Izdvojiti ime i prezime svakog studenta cije ime nije Marko, Veljko ili Ana. Rezultat urediti prema prezimenu
+u opadajucem poretku, a zatim prema imenu u rastucem poretku.
+*/
