b82e6e660773108362ee5221d22bc83535aa2074
Aca-S <a.stefanovic1999@gmail.com>
2024-10-16 19:59:20 +0200

    Added lecture 2 materials for intro to relational databases

---
+++ docs/resources/urbp/02_zadaci_za_vezbu.sql
@@ -0,0 +1,38 @@
+-- 1. Izdvojiti podatke o priznatim ispitima sa poznatom ocenom.
+
+
+/*
+2. Izdvojiti podatke o upisanim skolskim godinama studenata. Izdvojiti indeks,
+ime i prezime studenta, skolsku godinu i datum upisa godine. Rezultat
+urediti prema indeksu u opadajucem poretku i skolskoj godini u rastucem poretku.
+*/
+
+
+-- 3. Izdvojiti parove studenata koji su rodjeni u istom mestu. Izdvojiti indekse studenata.
+
+		
+/*
+4. Za svaki predmet izdvojiti podatke o ispitnim rokovima u kojima je predmet
+ponisten. Izdvojiti naziv predmeta, skolsku godinu u kojoj je odrzan ispitni rok
+i oznaku roka. Izdvojiti podatke i o predmetima ciji nijedan ispit nije ponisten.
+Upit napisati tako da nema ponavljanja redova u rezultatima.
+*/
+
+
+/*
+5. Za svaki predmet izdvojiti podatke o ispitnim rokovima u kojima je predmet
+ponisten. Izdvojiti naziv predmeta i naziv ispitnog roka. Izdvojiti podatke i 
+o predmetima ciji nijedan ispit nije ponisten. Upit napisati tako da nema 
+ponavljanja redova u rezultatima.
+*/
+
+
+/*
+6. Izdvojiti podatke o studentima i njihovim upisanim predmetima od 
+5, 10, 12 ili 25 espb ciji naziv pocinje sa Pr i sadrzi malo slovo o. 
+Izdvojiti podatke samo za skolske godine u intervalu od 2016/2017. do 2020/2021. 
+Izdvojiti indeks, ime, prezime studenta, skolsku godinu, naziv upisanog predmeta 
+i broj espb predmeta. Rezultat urediti prema indeks, skolskoj godini i oznaci predmeta. 
+*/
+
+
